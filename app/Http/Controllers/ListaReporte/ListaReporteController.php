858aa694c658c0ad19579687671aa8369b36f561
Francisco Garcia Hernandez <MaroGh@MacBook-Pro-de-Francisco.local>
2019-07-24 11:38:08 -0500

    Sammy Update

---
+++ app/Http/Controllers/ListaReporte/ListaReporteController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SAI\Http\Controllers\ListaReporte;
+
+use Illuminate\Http\Request;
+use SAI\Http\Controllers\Controller;
+use DB;
+use Illuminate\Support\Facades\Redirect;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class ListaReporteController extends Controller
+{
+    public function index($idlistaReporte)
+    {
+        $listasReportes = DB::table('tlv_1821_lr')->where('idlistaReportes',$idlistaReporte)->get();
+        return view('editarListaReporte')->with('listasReportes',$listasReportes);
+    }
+
+    public function guardarCambios(Request $request, $idlistaReporte)
+    {
+        if(isset($_POST['cancelar'])){
+            return Redirect::to('/consultadeReportes');
+        }else{
+            $data=array();
+            if($request->estatus != null){
+                $s = mb_convert_case($request->estatus, MB_CASE_UPPER, 'UTF-8');
+                $data['status']= $s;
+            }
+            $data['observaciones']=$request->observaciones;
+            $data['periodoAtencion']=$request->periodoAtencion;
+     
+            DB::table('tlv_1821_lr')->where('idlistaReportes',$idlistaReporte)->update($data);
+            Alert::success('Reporte Actualizado', 'Reporte Actualizado Correctamente');
+            return Redirect::to('/consultadeReportes');
+        }
+       
+    }
+
+
+    
+}
